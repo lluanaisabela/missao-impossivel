bfd851793f88a4b6de29c221ca2ae9759739840a
Mateus Chines <mateuschines>
2023-08-12 09:49:07 -0300

    Aula 07-08

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// https://github.com/profchines
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Primeira aula de react</p>
+    </>
+  );
+}
+
+export default App;

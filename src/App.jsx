f831df82ec05b150d6c9d0b8f6be981c37b28d4a
Mateus Chines <mateuschines>
2023-08-21 22:24:12 -0300

    Aula 21-08

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Header } from './components/Header'
+
+import banner from './imagens/banner.jpg'
+import styles from './styles.module.css'
+import './styles/global.css'
+// npm install
+// npm run start
 
 // https://github.com/profchines
 function App() {
+
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
+      <img width="100%" src={banner} />
+      <h1 className={styles.title}>Hello World</h1>
+      <h1
+        style={{
+          color: 'blue',
+          backgroundColor: 'red',
+        }}
+      >5 min pro intervalo</h1>
       <p>Primeira aula de react</p>
     </>
   );
